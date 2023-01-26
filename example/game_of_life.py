0536d6f6b159dd0bb7f9d0fb776bbc9a0659ae3f
Zhe Wang <wangzhetju@gmail.com>
2023-01-26 15:39:21 +0000

    update build

--- example/game_of_life.py
+++ example/game_of_life.py
@@ -2,8 +2,9 @@ from pyca.universe2d import Universe2D
 from pyca.celltype import GameOfLife
 from pyca.observer import plot2d_animate
 
-universe = Universe2D(50, 50)
-universe.register_cell_type(GameOfLife, 'full_fill', life_prob=0.3)
+universe = Universe2D(100, 100)
+universe.register_cell_type(GameOfLife, 'full_fill', life_prob=0.5)
 universe.initialize()
 universe.compute(500)
-plot2d_animate(universe, write_to='../assets/game_of_life.gif')
+# plot2d_animate(universe, write_to='../assets/game_of_life.gif')
+plot2d_animate(universe, show=True)
